b3ee8afee7498ad1fe964d0211f5f270116269e8
cdeon <clement.deon@data-publica.com>
2016-03-06 19:05:22 +0100

    Add gif and jpeg to file loader

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ var config = {
         loader:'style!css!postcss!sass'
       },
       {
-        test: /\.(jpg|png|svg|ico|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(gif|jpeg|jpg|png|svg|ico|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "file?name=[path][name].[ext]?[hash]"
       }
     ]
